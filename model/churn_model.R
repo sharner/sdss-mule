2ed83d070e518d3ef49780e9056de16d9c000884
Soren Harner <soeren@gmail.com>
2018-05-22 14:43:59 -0700

    Add an ROC curve to the model.

--- model/churn_model.R
+++ model/churn_model.R
@@ -7,6 +7,7 @@ library(rsample)
 library(recipes)
 library(yardstick)
 library(corrr)
+library(plotROC)
 
 # Load and prune data
 churn_data_raw <- read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
@@ -21,9 +22,9 @@ glimpse(churn_data_tbl)
 set.seed(100)
 train_test_split <- initial_split(churn_data_tbl, prop = 0.8)
 train_tbl <- training(train_test_split)
-test_tbl  <- testing(train_test_split) 
+test_tbl  <- testing(train_test_split)
 
-# Determine if log transformation improves correlation 
+# Determine if log transformation improves correlation
 # between TotalCharges and Churn
 train_tbl %>%
   select(Churn, TotalCharges) %>%
@@ -36,7 +37,7 @@ train_tbl %>%
   fashion()
 
 
-# Create recipe
+# Create recipes
 rec_obj <- recipe(Churn ~ ., data = train_tbl) %>%
   step_discretize(tenure, options = list(cuts = 6)) %>%
   step_log(TotalCharges) %>%
@@ -56,33 +57,28 @@ y_test_vec  <- ifelse(pull(test_tbl, Churn) == "Yes", 1, 0)
 # Defining the model
 
 # Building our Artificial Neural Network
-model_keras <- keras_model_sequential() %>% 
-  
+
+model_keras <- keras_model_sequential() %>%
   # First hidden layer
   layer_dense(
-    units              = 16, 
-    kernel_initializer = "uniform", 
-    activation         = "relu", 
-    input_shape        = ncol(x_train_tbl)) %>% 
-  
+    units              = 16,
+    kernel_initializer = "uniform",
+    activation         = "relu",
+    input_shape        = ncol(x_train_tbl)) %>%
   # Dropout to prevent overfitting
   layer_dropout(rate = 0.1) %>%
-  
   # Second hidden layer
   layer_dense(
-    units              = 16, 
-    kernel_initializer = "uniform", 
-    activation         = "relu") %>% 
-  
+    units              = 16,
+    kernel_initializer = "uniform",
+    activation         = "relu") %>%
   # Dropout to prevent overfitting
   layer_dropout(rate = 0.1) %>%
-  
   # Output layer
   layer_dense(
-    units              = 1, 
-    kernel_initializer = "uniform", 
-    activation         = "sigmoid") %>% 
-  
+    units              = 1,
+    kernel_initializer = "uniform",
+    activation         = "sigmoid") %>%
   # Compile ANN
   compile(
     optimizer = 'adam',
@@ -90,25 +86,29 @@ model_keras <- keras_model_sequential() %>%
     metrics   = c('accuracy')
   )
 
-# Fit the keras model to the training data
+# Fit the model to traing data
+
 history <- fit(
-  object           = model_keras, 
-  x                = as.matrix(x_train_tbl), 
+  object           = model_keras,
+  x                = as.matrix(x_train_tbl),
   y                = y_train_vec,
-  batch_size       = 50, 
-  epochs           = 35,
+  batch_size       = 50,
+  epochs           = 10,
   validation_split = 0.30
 )
 
 # Class predictions
-yhat_keras_class_vec <- predict_classes(object = model_keras, x = as.matrix(x_test_tbl)) %>%
+yhat_keras_class_vec <- predict_classes(object = model_keras,
+                                        x = as.matrix(x_test_tbl)) %>%
   as.vector()
 
 # Predicted class probabilities
-yhat_keras_prob_vec  <- predict_proba(object = model_keras, x = as.matrix(x_test_tbl)) %>%
+yhat_keras_prob_vec  <- predict_proba(object = model_keras,
+                                      x = as.matrix(x_test_tbl)) %>%
   as.vector()
 
 # Format test data and predictions for yardstick metrics
+
 estimates_keras_tbl <- tibble(
   truth      = as.factor(y_test_vec) %>% fct_recode(yes = "1", no = "0"),
   estimate   = as.factor(yhat_keras_class_vec) %>% fct_recode(yes = "1", no = "0"),
@@ -117,14 +117,18 @@ estimates_keras_tbl <- tibble(
 estimates_keras_tbl
 options(yardstick.event_first = FALSE)
 
-# Confusion Table
-estimates_keras_tbl %>% conf_mat(truth, estimate)
-
 # Accuracy
 estimates_keras_tbl %>% metrics(truth, estimate)
 
+# Confusion Table
+estimates_keras_tbl %>% conf_mat(truth, estimate)
+
 # AUC
 estimates_keras_tbl %>% roc_auc(truth, class_prob)
+
+# Plot the ROC
+roc_plot <- ggplot(estimates_keras_tbl, aes(d = truth, m = class_prob)) + geom_roc()
+roc_plot
 
 # Precision
 tibble(
@@ -136,8 +140,12 @@ tibble(
 estimates_keras_tbl %>% f_meas(truth, estimate, beta = 1)
 
 # Save the test set and the model
+
 save_model_hdf5(model_keras, filepath="/tmp/mule-pipeline/churn_model.h5",
                 overwrite = TRUE, include_optimizer = TRUE)
+
+
 churn_data_raw_tbl <- bake(rec_obj, newdata = churn_data_raw %>% select (-customerID)) %>% select(-Churn)
 churn_data_raw_tbl <- bind_cols(churn_data_raw %>% select(customerID), churn_data_raw_tbl)
+
 write_csv(churn_data_raw_tbl, path="/tmp/mule-pipeline/churn_data_raw_tbl.csv", col_names=FALSE)
